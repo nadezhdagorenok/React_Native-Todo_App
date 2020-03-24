4a698029538ea3194aa111e489ef64cc1e609806
nadzeyaharanok <N.Haranok@sam-solutions.com>
2020-03-24 16:26:56 +0300

    creating app components

---
+++ src/AddTodo.js
@@ -0,0 +1,49 @@
+import React, {useState} from 'react'
+import {View, StyleSheet, TextInput, Button, Alert} from 'react-native'
+
+const styles = StyleSheet.create({
+    block: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginBottom: 15,
+    },
+    input: {
+        width: '70%',
+        padding: 10,
+        borderStyle: 'solid',
+        borderBottomWidth: 2,
+        borderBottomColor: '#3949ab'
+
+    },
+  
+
+})
+
+export const AddTodo = ({ onSubmit }) => {
+    const [value, setValue] = useState('');
+
+    const pressHandler = (e) => {
+        if(value.trim()){
+            onSubmit(value);
+            setValue('');
+            return;
+        }
+        Alert.alert('Error! The deal name can not be empty!!!')
+    }
+
+    return (
+    <View style={styles.block}>
+        <TextInput 
+          style={styles.input} 
+          onChangeText={setValue}
+          value={value}
+          placeholder="Type deal name..."
+          autoCorrect={false}
+          autoCapitalize='none'
+          keyboardType="number-pad"
+          />
+        <Button title={'Add'} onPress={pressHandler} />
+    </View>
+    )
+}
---
+++ src/Todo.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {View, StyleSheet, Text, TouchableOpacity} from 'react-native'
+
+
+const styles = StyleSheet.create({
+    todo: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 15,
+        borderWidth: 1,
+        borderRadius: 5,
+        borderColor: '#eee',
+        marginBottom: 10,
+    },
+})
+
+export const Todo = ({todo, onRemove}) => {
+    return (
+        <TouchableOpacity 
+          activeOpacity={0.5} 
+          onPress={()=>console.log('Pressed', todo.id)}
+          onLongPress={onRemove.bind(null, todo.id)}
+          >
+          <View style={styles.todo}>
+            <Text>{todo.title}</Text>
+          </View>
+        </TouchableOpacity>
+    )
+}
